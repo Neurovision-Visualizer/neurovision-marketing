4d6a39e454b3d79bab4e3f1e06dd08b9b9744a85
Jason Gayle <jasonblue1626@gmail.com>
2022-05-23 19:26:40 -0500

    Fixed for deploy

--- pages/index.js
+++ pages/index.js
@@ -72,9 +72,9 @@ export default function Home() {
               height="315"
               src="https://www.youtube.com/embed/DMDECliYMWM"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
+              allowFullScreen
             ></iframe>
           </article>
         </section>
